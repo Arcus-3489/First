fa2e55ab8b00a4ff12b8927008df878c276618bc
Arcus-3489 <75382728+Arcus-3489@users.noreply.github.com>
2020-12-04 14:52:19 +0900

    Add files via upload

--- Forum.php
+++ Forum.php
@@ -3,29 +3,29 @@
 
 // DB接続設定
 $dsn = 'データーベース名';
-	$user = 'ユーザー名';
-	$password = 'パスワード';
-    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+$user = 'ユーザー名';
+$password = 'パスワード';
+$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
 // テーブル作成
-    
+
 $sql = "CREATE TABLE IF NOT EXISTS tbtest"
-." ("
-. "id INT AUTO_INCREMENT PRIMARY KEY,"
-. "name char(32),"
-. "comment TEXT,"
-. "date TEXT,"
-. "password TEXT"
-.");";
+    . " ("
+    . "id INT AUTO_INCREMENT PRIMARY KEY,"
+    . "name char(32),"
+    . "comment TEXT,"
+    . "date TEXT,"
+    . "password TEXT"
+    . ");";
 $stmt = $pdo->query($sql);
-$sql ='SHOW TABLES';
-	$result = $pdo -> query($sql);
-	foreach ($result as $row){
-		echo $row[0];
-		echo '<br>';
-	}
-    echo "<hr>";
-    
+$sql = 'SHOW TABLES';
+$result = $pdo->query($sql);
+foreach ($result as $row) {
+    echo $row[0];
+    echo '<br>';
+}
+echo "<hr>";
+
 $newname = null;
 $newcomment = null;
 $id = null;
@@ -39,7 +39,7 @@ if (isset($_POST["submit2"]) && strlen($_POST["pw3"])) {
     $sql = "select * from tbtest where id=$id";
     $stmt = $pdo->query($sql);
     //$results = $stmt->fetchAll();
-    foreach ($stmt as $row){
+    foreach ($stmt as $row) {
         //$rowの中にはテーブルのカラム名が入る
         $oldpw = $row['password'];
     }
@@ -49,7 +49,7 @@ if (isset($_POST["submit2"]) && strlen($_POST["pw3"])) {
             $sql = "select * from tbtest where id=$id";
             $stmt = $pdo->query($sql);
             //$results = $stmt->fetchAll();
-            foreach ($stmt as $row){
+            foreach ($stmt as $row) {
                 //$rowの中にはテーブルのカラム名が入る
                 $id = $row['id'];
                 $newname = $row['name'];
@@ -64,74 +64,75 @@ if (isset($_POST["submit2"]) && strlen($_POST["pw3"])) {
 }
 
 ?>
-    <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>mission_5-01</title>
-    </head>
-    <body>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>mission_5-01</title>
+</head>
+
+<body>
     <f1> ただの掲示板</f1>
-        <form action = "" method = "post">
-            名前
-            <input type = "text" name = "str0"
-            value = "<?php 
-            ini_set('display_errors', 0);
-            echo $newname;
-            ?>">
-            <br>
-            
-            コメント
-            <input type = "text" name = "str1"
-            value = "<?php 
-            ini_set('display_errors', 0);
-            echo $newcomment;
-            ?>">
-            <br>
+    <form action="" method="post">
+        名前
+        <input type="text" name="str0" value="
+        <?php
+        ini_set('display_errors', 0);
+        echo $newname;
+        ?>">
+        <br>
 
-            パスワード
-            <input type = "text" name = "pw1">
-            <input type = "submit" name = "submit0">
-            <br><br>
-            
-            削除対象番号
-            <input type = "number" name = "num"
-            value = "">
-            <br>
-            削除用パスワード
-            <input type = "text" name = "pw2">
-            <input type = "submit" name = "submit1"
-            value = "削除">
-            <br><br>
-            
-            編集対象番号
-            <input type = "number" name = "num1"
-            value = "">
-            <br>
-            編集用パスワード
-            <input type = "text" name = "pw3">
-            <input type = "submit" name = "submit2"
-            value = "編集">
-                
-            <input type = "hidden" name = "num2" value = "<?php
-            echo $id;
-            ?>">
-            <input type = "hidden" name = "num3" value = "<?php
-            echo $pw;
-            ?>">
-            <br>
-            ※編集する場合は、編集対象番号と編集用パスワードを入力すると入力した名前とコメントが
-            <br>
-            フォーム内に表示されます。編集を終えたら同一パスワードを入力して「送信」をクリックしてください。
-            <br><br>
-            ＜掲示板＞<br>
-            No.／名前／コメント／投稿日時
-            <br><br>
+        コメント
+        <input type="text" name="str1" value="
+        <?php
+        ini_set('display_errors', 0);
+        echo $newcomment;
+        ?>">
+        <br>
 
-        </form>
-    
-    </body>
-    </html>
-    
+        パスワード
+        <input type="text" name="pw1">
+        <input type="submit" name="submit0">
+        <br><br>
+
+        削除対象番号
+        <input type="number" name="num" value="">
+        <br>
+        削除用パスワード
+        <input type="text" name="pw2">
+        <input type="submit" name="submit1" value="削除">
+        <br><br>
+
+        編集対象番号
+        <input type="number" name="num1" value="">
+        <br>
+        編集用パスワード
+        <input type="text" name="pw3">
+        <input type="submit" name="submit2" value="編集">
+
+        <input type="hidden" name="num2" value="
+        <?php
+        echo $id;
+        ?>">
+        <input type="hidden" name="num3" value="
+        <?php
+        echo $pw;
+        ?>">
+        <br>
+        ※編集する場合は、編集対象番号と編集用パスワードを入力すると入力した名前とコメントが
+        <br>
+        フォーム内に表示されます。編集を終えたら同一パスワードを入力して「送信」をクリックしてください。
+        <br><br>
+        ＜掲示板＞<br>
+        No.／名前／コメント／投稿日時
+        <br><br>
+
+    </form>
+
+</body>
+
+</html>
+
 <?php
 if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
     if ($_POST["num2"] != null) {
@@ -142,14 +143,14 @@ if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
         $sql = "select * from tbtest where id=$id";
         $stmt = $pdo->query($sql);
         //$results = $stmt->fetchAll();
-        foreach ($stmt as $row){
+        foreach ($stmt as $row) {
             //$rowの中にはテーブルのカラム名が入る
             $oldpw = $row['password'];
         }
 
-    
+
         if ($pw == $oldpw) {
-            
+
             $name = $_POST["str0"];
             $comment = $_POST["str1"]; //変更したい名前、変更したいコメントは自分で決めること
             $date = date("Y/m/d H:i:s");
@@ -165,7 +166,6 @@ if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
             $stmt->execute();
         } else {
         }
-
     } else {
 
         $name = $_POST["str0"];
@@ -173,40 +173,37 @@ if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
         $date = date("Y/m/d H:i:s");
         $pw = $_POST["pw1"];
         // レコード挿入    
-        $sql = $pdo -> prepare("INSERT INTO tbtest (name, comment, date, password) 
+        $sql = $pdo->prepare("INSERT INTO tbtest (name, comment, date, password) 
         VALUES (:name, :comment, :date, :password)");
-        $sql -> bindParam(':name', $name, PDO::PARAM_STR);
-        $sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
-        $sql -> bindParam(':date', $date, PDO::PARAM_STR);
-        $sql -> bindParam(':password', $pw, PDO::PARAM_STR);
-        $sql -> execute();
+        $sql->bindParam(':name', $name, PDO::PARAM_STR);
+        $sql->bindParam(':comment', $comment, PDO::PARAM_STR);
+        $sql->bindParam(':date', $date, PDO::PARAM_STR);
+        $sql->bindParam(':password', $pw, PDO::PARAM_STR);
+        $sql->execute();
     }
-
-
-} else if (isset($_POST["submit1"]) && strlen($_POST["pw2"]) ) {
+} else if (isset($_POST["submit1"]) && strlen($_POST["pw2"])) {
     $id = $_POST["num"];
     $pw = $_POST["pw2"];
     $oldpw = "";
 
-// パスワードが一致していたら、該当の投稿を削除
+    // パスワードが一致していたら、該当の投稿を削除
     $sql = "select * from tbtest where id=$id";
     $stmt = $pdo->query($sql);
     //$results = $stmt->fetchAll();
-    foreach ($stmt as $row){
-		//$rowの中にはテーブルのカラム名が入る
+    foreach ($stmt as $row) {
+        //$rowの中にはテーブルのカラム名が入る
         $oldpw = $row['password'];
     }
-    
-  if ($pw == $oldpw) {
-    $sql = "delete from tbtest where id=$id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    }
 
+    if ($pw == $oldpw) {
+        $sql = "delete from tbtest where id=$id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
 }
 
-    /* // レコード編集
+/* // レコード編集
     $id = 1; //変更する投稿番号
 	$name = "rain";
 	$comment = "Hello PHP"; //変更したい名前、変更したいコメントは自分で決めること
@@ -219,7 +216,7 @@ if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
 
     */
 
-    /*
+/*
     // レコード削除
     $id = 3;
 	$sql = 'delete from tbtest where id=:id';
@@ -232,22 +229,22 @@ if (isset($_POST["submit0"]) && strlen($_POST["pw1"])) {
      //【！この SQLは tbtest テーブルを削除します！】
 		$sql = 'DROP TABLE tbtest';
         $stmt = $pdo->query($sql);
-  */  
-    
-    // レコード表示
- 
-    $sql = 'SELECT * FROM tbtest';
-	$stmt = $pdo->query($sql);
-	$results = $stmt->fetchAll();
-    foreach ($results as $row){
-		//$rowの中にはテーブルのカラム名が入る
-		echo $row['id'].',';
-		echo $row['name'].',';
-        echo $row['comment'].',';
-        echo $row['date'].'';
-        //echo $row['password'].'<br>';
-        echo '<br>';
-	echo "<hr>";
-	}
+  */
+
+// レコード表示
+
+$sql = 'SELECT * FROM tbtest';
+$stmt = $pdo->query($sql);
+$results = $stmt->fetchAll();
+foreach ($results as $row) {
+    //$rowの中にはテーブルのカラム名が入る
+    echo $row['id'] . ',';
+    echo $row['name'] . ',';
+    echo $row['comment'] . ',';
+    echo $row['date'] . '';
+    //echo $row['password'].'<br>';
+    echo '<br>';
+    echo "<hr>";
+}
 
 ?>
